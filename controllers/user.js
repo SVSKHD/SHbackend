0b683a9748e20cb8caf4765134d987b8f4629a83
hithesh svsk <8svskhd@gmail.com>
2021-02-14 10:38:54 +0530

    user update

--- controllers/user.js
+++ controllers/user.js
@@ -47,44 +47,66 @@ User.findOne({username}).exec((err,userFromDB)=>{
 }
 )}
 
-exports.update = (req,res) =>{
-    let form = new formidable.IncomingForm()
-    form.keepExtensions = true 
-    form.parse(req,(err,fields,files)=>{
-        if(err){
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtension = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
             return res.status(400).json({
-                error:"Photo could not be uploaded"
-            })
+                error: 'Photo could not be uploaded'
+            });
         }
-        let user = req.profile
-        user = _.extend(user,fields)
-
-        if(fields.password && fields.password.length<6){
+ 
+        let user = req.profile;
+        // user's existing role and email before update
+        let existingRole = user.role;
+        let existingEmail = user.email;
+ 
+        if (fields && fields.username && fields.username.length > 12) {
             return res.status(400).json({
-                error:"Password should be min 6 characters long"
-            })
+                error: 'Username should be less than 12 characters long'
+            });
         }
-
-        if(files.photo){
-            if(files.photo.size>1000000){
-                error:"Image should be less than 1Mb"
+ 
+        if (fields.username) {
+            fields.username = slugify(fields.username).toLowerCase();
         }
-        user.photo.data = fs.readFileSync(files.photo.path)
-        user.photo.contentType = files.photo.type
+ 
+        if (fields.password && fields.password.length < 6) {
+            return res.status(400).json({
+                error: 'Password should be min 6 characters long'
+            });
         }
-        
-        user.save((err,result)=>{
-            if(err){
+ 
+        user = _.extend(user, fields);
+        // user's existing role and email - dont update - keep it same
+        user.role = existingRole;
+        user.email = existingEmail;
+ 
+        if (files.photo) {
+            if (files.photo.size > 10000000) {
                 return res.status(400).json({
-                    error:errorHandler(err)
-                })
+                    error: 'Image should be less than 1mb'
+                });
             }
-            user.hashed_password = undefined
-            res.json(user)
-        })
-    })
-}
-
+            user.photo.data = fs.readFileSync(files.photo.path);
+            user.photo.contentType = files.photo.type;
+        }
+ 
+        user.save((err, result) => {
+            if (err) {
+                console.log('profile udpate error', err);
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            user.hashed_password = undefined;
+            user.salt = undefined;
+            user.photo = undefined;
+            res.json(user);
+        });
+    });
+};
 
 exports.photo = (req, res) => {
     const username = req.params.username;
